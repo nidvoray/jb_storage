aaf47babb2dfafce269cbe95294345ddded4cd48
Andrey Egorov <andrey.egorov@gs-labs.ru>
2023-02-24 13:45:11 +0300

    VolumeImpl: store implementation-type root

--- storage/source/VolumeImpl.cpp
+++ storage/source/VolumeImpl.cpp
@@ -7,78 +7,71 @@
 namespace jb_storage
 {
 
-	namespace
+	class VolumeImpl::Node final : public INode
 	{
+	private:
+		Value							_value;
+		std::map<std::string, INodePtr>	_children;
+		MutexType						_lock;
 
-		class Node final : public INode
+	public:
+		std::optional<Value> GetValue() const override
+		{ return _value; }
+
+		bool GrowBranchAndSetValue(const utility::Path& path, const Value& value) override
 		{
-			using NodePtr = std::shared_ptr<Node>;
-
-		private:
-			Value							_value;
-			std::map<std::string, INodePtr>	_children;
-			MutexType						_lock;
-
-		public:
-			std::optional<Value> GetValue() const override
-			{ return _value; }
-
-			bool GrowBranchAndSetValue(const utility::Path& path, const Value& value) override
+			if (!path.IsEmpty())
 			{
-				if (!path.IsEmpty())
-				{
-					auto key{ path.begin() };
+				auto key{ path.begin() };
 
-					const auto new_subbranch{ std::make_shared<Node>() };
-					const auto new_subbranch_name{ *key++ };
+				const auto new_subbranch{ std::make_shared<Node>() };
+				const auto new_subbranch_name{ *key++ };
 
-					auto tail{ new_subbranch };
-					for (const auto end{ path.end() }; key != end; ++key)
-						tail = tail->SetChild(*key, std::make_shared<Node>());
+				auto tail{ new_subbranch };
+				for (const auto end{ path.end() }; key != end; ++key)
+					tail = tail->SetChild(*key, std::make_shared<Node>());
 
-					tail->_value = value;
+				tail->_value = value;
 
-					SetChild(new_subbranch_name, new_subbranch);
-				}
-				else
-					_value = value;
-
-				return true;
+				SetChild(new_subbranch_name, new_subbranch);
 			}
+			else
+				_value = value;
 
-			INodePtr GetChild(const std::string& name) const override
-			{
-				const auto child{ _children.find(name) };
-				return child != _children.end() ? child->second : nullptr;
-			}
+			return true;
+		}
 
-			bool DeleteChild(const std::string& name) override
-			{ return _children.erase(name) != 0; }
+		INodePtr GetChild(const std::string& name) const override
+		{
+			const auto child{ _children.find(name) };
+			return child != _children.end() ? child->second : nullptr;
+		}
 
-			void lock() override
-			{ _lock.lock(); }
+		bool DeleteChild(const std::string& name) override
+		{ return _children.erase(name) != 0; }
 
-			void unlock() override
-			{ _lock.unlock(); }
+		void lock() override
+		{ _lock.lock(); }
 
-			void lock_shared() override
-			{ _lock.lock_shared(); }
+		void unlock() override
+		{ _lock.unlock(); }
 
-			void unlock_shared() override
-			{ _lock.unlock_shared(); }
+		void lock_shared() override
+		{ _lock.lock_shared(); }
 
-		private:
-			NodePtr SetChild(const std::string& name, const NodePtr& child)
-			{
-				_children.insert_or_assign(name, child);
-				return child;
-			}
-		};
+		void unlock_shared() override
+		{ _lock.unlock_shared(); }
 
-	}
+	private:
+		NodePtr SetChild(const std::string& name, const NodePtr& child)
+		{
+			_children.insert_or_assign(name, child);
+			return child;
+		}
+	};
 
 	VolumeImpl::VolumeImpl()
-		: BaseImpl{ std::make_shared<Node>() }
+		: VolumeImpl{ std::make_shared<Node>() }
 	{ }
 
 	void VolumeImpl::AddRef()
@@ -104,5 +97,9 @@ namespace jb_storage
 			// TODO: save data here
 		}
 	}
+
+	VolumeImpl::VolumeImpl(const NodePtr& root) noexcept
+		: BaseImpl(root), _root(root)
+	{ }
 
 }
--- storage/source/VolumeImpl.h
+++ storage/source/VolumeImpl.h
@@ -12,8 +12,11 @@ namespace jb_storage
 
 	class VolumeImpl final : public BaseImpl
 	{
+		class Node;
+		using NodePtr = std::shared_ptr<Node>;
+
 	private:
-		INodePtr				_root;
+		NodePtr					_root;
 		std::atomic<unsigned>	_refcounter;
 
 	public:
@@ -26,6 +29,9 @@ namespace jb_storage
 
 		void Load(std::istream& is) const;
 		void Save(std::ostream& os) const;
+
+	private:
+		explicit VolumeImpl(const NodePtr& root) noexcept;
 	};
 
 	using VolumeImplPtr = std::shared_ptr<VolumeImpl>;
