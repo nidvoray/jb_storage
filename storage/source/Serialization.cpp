cfce96834b9598eab7213f41e317225839ff83ec
Andrey Egorov <andrey.yegorof@gmail.com>
2023-03-22 03:35:18 +0300

    Serialization: handle std::variant using index()

--- storage/source/Serialization.cpp
+++ storage/source/Serialization.cpp
@@ -6,54 +6,44 @@ namespace jb_storage::utility
 	namespace
 	{
 
-		enum class ValueType
+		template < typename... T >
+		std::variant<T...> Deserialize(std::istream& is, const std::variant<T...>*)
 		{
-			Uint32,
-			Uint64,
-			Float,
-			Double,
-			String,
-			Blob,
-			Monostate
-		};
+			using Value = std::variant<T...>;
+
+			const size_t index{ utility::Deserialize<uint8_t>(is) };
+			if (index >= sizeof...(T))
+				throw std::out_of_range{ "index " + std::to_string(index) + " out of range" };
+
+			static Value (* const creators[])(std::istream&)
+			{
+				[](std::istream& is)
+				{
+					if constexpr (!std::is_same_v<T, std::monostate>)
+						return Value{ utility::Deserialize<T>(is) };
+					else
+						return Value{ };
+				}...
+			};
+
+			return creators[index](is);
+		}
 
 	}
 
 	void Serialize(const Value& val, std::ostream& os)
 	{
+		Serialize(static_cast<uint8_t>(val.index()), os);
 		std::visit([&os](const auto& arg)
 		{
 			using T = std::decay_t<decltype(arg)>;
-			constexpr auto type
-			{
-				std::is_same_v<T, uint32_t> ? ValueType::Uint32 :
-				std::is_same_v<T, uint64_t> ? ValueType::Uint64 :
-				std::is_same_v<T, float> ? ValueType::Float :
-				std::is_same_v<T, double> ? ValueType::Double :
-				std::is_same_v<T, std::string> ? ValueType::String :
-				std::is_same_v<T, Blob> ? ValueType::Blob :
-				ValueType::Monostate
-			};
-
-			Serialize(static_cast<uint8_t>(type), os);
-			if (type != ValueType::Monostate)
+			if constexpr (!std::is_same_v<T, std::monostate>)
 				Serialize(arg, os);
 		}, val);
 	}
 
 	template < >
 	Value Deserialize<Value>(std::istream& is)
-	{
-		const auto type{ static_cast<ValueType>(Deserialize<uint8_t>(is)) };
-
-		return
-			type == ValueType::Uint32 ? Value{ Deserialize<uint32_t>(is) } :
-			type == ValueType::Uint64 ? Value{ Deserialize<uint64_t>(is) } :
-			type == ValueType::Float ? Value{ Deserialize<float>(is) } :
-			type == ValueType::Double ? Value{ Deserialize<double>(is) } :
-			type == ValueType::String ? Value{ Deserialize<std::string>(is) } :
-			type == ValueType::Blob ? Value{ Deserialize<Blob>(is) } :
-			Value{ };
-	}
+	{ return Deserialize(is, static_cast<const Value*>(nullptr)); }
 
 }
